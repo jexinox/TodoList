deadf72039ad0df93d6a9075ff9ac728239b222b
jexinox <jexinox@gmail.com>
2022-07-09 16:30:29 +0500

    Added unimplemented get method Added pagination params

--- TodoList/Controllers/TodoController.cs
+++ TodoList/Controllers/TodoController.cs
@@ -1,8 +1,14 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using TodoList.Utils;
 
 namespace TodoList.Controllers;
 
+[Route("/api/todo")]
 public class TodoController : Controller
 {
-    
+    [HttpGet]
+    public IActionResult ReadTodo(TodoPaginationParameters parameters)
+    {
+        throw new NotImplementedException();
+    }
 }
---
+++ TodoList/Utils/TodoPaginationParameters.cs
@@ -0,0 +1,13 @@
+﻿namespace TodoList.Utils;
+
+public class TodoPaginationParameters
+{
+    private const int max_page_size = 50;
+    public int PageNumber { get; set; } = 1;
+    private int pageSize = 10;
+    public int PageSize
+    {
+        get => pageSize;
+        set => pageSize = value > max_page_size ? max_page_size : value;
+    }
+}
