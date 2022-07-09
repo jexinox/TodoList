9de8c3fe7c3d17f6d7d79735c63881a42505c910
jexinox <jexinox@gmail.com>
2022-07-09 14:35:14 +0500

    Start

---
+++ TodoList/Program.cs
@@ -0,0 +1,19 @@
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllersWithViews();
+
+var app = builder.Build();
+
+if (!app.Environment.IsDevelopment())
+{
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+app.UseRouting();
+app.MapControllers();
+
+app.MapFallbackToFile("index.html");
+
+app.Run();
